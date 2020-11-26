58e0308075d8f05c222dcbb8a6ad3138c4f23ccf
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-26 09:25:39 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.MultiDimArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //one DimensionalArray -> one direction to fill, one indexsation, one numeratin of indexes
+            //twoDimensionalArray- > as table-> we ha ve rows ans colomns, matrix, another example, 
+            //excel is two dimensional array , we have two directions of indexing, 
+            //two dimesnionalAray-We have two numbers that identify a given cell ;
+            //TryDimensionalArray --example a Cube ->  we have tree dimensions, three numerations of indexes 
+            //on which we can walk on -> every cell has 3 indexes;//every cell has indexing of three dimensions
+            //multidimenional array = array with more than inclusive 2 Dimenensions//more than one Dimension;
+            //twoDimensional Array is called Matrix, or Tables/--example Excel Table is two dimesnional Array/
+            //three DimensionalArray indexsation of cells is into three directions , indexing by rows, indexing by colomns, 
+            //indexing by Depth--example of Three dimensional Array is th cube !!!!
+            //
+
+            //Example oneDimesionalArray :
+            int[] numbers = new int[5]; //this is one dimensional Array consisting of 5 elements;
+            //Example of twoDimesnionalArray called, matrix, or table, example is an excel table;
+            int[,] numberS = new int[5,10]; //two dimesnional array,  5rows and  10 columns;
+            //the comma into [ , ] says that the array is here two dimesnional ! 
+
+
+
+
+        }
+    }
+}
